a5b6cdb125e3d62fc6d78ac1537e7b47c5c2e344
basestation5 <Jeremy@jbookpro.local>
2017-06-28 20:38:17 -0400

    couple small updates

--- src/test/java/com/thedeathstar/util/CukesRunnerTest.java
+++ src/test/java/com/thedeathstar/util/CukesRunnerTest.java
@@ -8,7 +8,7 @@ import cucumber.api.CucumberOptions;
  * Created by Jeremy on 6/27/17.
  */
 @RunWith(Cucumber.class)
-@CucumberOptions( plugin = { "com.consol.citrus.cucumber.CitrusReporter" } , features = "src/test/resources")
+@CucumberOptions( plugin = { "com.consol.citrus.cucumber.CitrusReporter" } , features = "src/test/resources/features")
 public class CukesRunnerTest {
 
 
